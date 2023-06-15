71730760d561fc8f882c8d72d369c3b9175c9f87
Amanda Sardella <amandansardella@gmail.com>
2023-06-15 01:57:07 -0300

    Assignment 2 DressStore Application - 1st Commit

---
+++ Backend_DressStore/app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //add the mongodb link+password+nameOfDatabase (before the question mark?)
+  url: "mongodb+srv://amandansardella:FLCcinspEkPdBKqN@cluster0.ppqgmoc.mongodb.net/DressStore?retryWrites=true&w=majority",
+};
